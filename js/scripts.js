ceae53e0fa0e57d601bec7ab3f5aff881ce94258
Dan Kiss and Margaret Berry <margaret@>
2017-08-15 12:03:36 -0700

    add js to replace one letter with dash

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,34 @@
 $(document).ready(function() {
   $(".sentence").submit(function(event) {
     event.preventDefault();
+    var userInputInJs = $("input.userInput").val();
+
+    // alert(userInputInJs);
+      $("#sentence1").text(userInputInJs);
+      $(".sentence").hide();
+      $("#puzzle").show();
+      newUserInputSlice1 = userInputInJs.slice();
+      // alert(newUserInputSlice1.length);
+      newUserInputSlice1.length;
+
+      for (index=0; newUserInputSlice1.length > index; index +=1 ) {
+        var letter = newUserInputSlice1.charAt(index);
+        // alert(letter);
+        if (letter === 'a') {
+          letter = letter.replace(/A/i, '-');
+          alert(letter);
+          console.log(letter);
+        }
+      }
+
+      newUserInputSlice1.charAt
+
+
+    $("#return").click(function(){
+      $("#puzzle").hide();
+      $(".sentence").show();
+
+
+    });
   });
 });
