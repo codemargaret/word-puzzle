6793c7c8e54b84b404bdeea85fbbf856275997e8
Dan Kiss and Margaret Berry <margaret@>
2017-08-15 14:57:28 -0700

    change if/else statements so more focus on looking at whole string of text at once as a single array

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,29 @@
 $(document).ready(function() {
   $(".sentence").submit(function(event) {
-    event.preventDefault();
     var userInputInJs = $("input.userInput").val();
+    var finalPuzzle =[];
+    event.preventDefault();
 
-    // alert(userInputInJs);
-      $("#sentence1").text(userInputInJs);
+      // $("#sentence1").text(userInputInJs);
       $(".sentence").hide();
       $("#puzzle").show();
-      newUserInputSlice1 = userInputInJs.slice();
-      // alert(newUserInputSlice1.length);
-      newUserInputSlice1.length;
+      // newUserInputSlice1 = userInputInJs.slice();
 
-      for (index=0; newUserInputSlice1.length > index; index +=1 ) {
-        var letter = newUserInputSlice1.charAt(index);
-        // alert(letter);
-        if (letter === 'a') {
-          letter = letter.replace(/A/i, '-');
-          alert(letter);
-          console.log(letter);
+
+      for (var index=0; userInputInJs.length > index; index +=1 ) {
+        var place = userInputInJs[index];
+        if (place=== 'a' || place === 'e' || place === 'i' ||  place === 'o' || place === 'u') {
+          finalPuzzle[index]='-';
+
+        }  else {
+            finalPuzzle[index]=place;
+        } if (index === userInputInJs.length-1)  {
+          $("#sentence1").text(finalPuzzle.join(''));
         }
       }
 
-      newUserInputSlice1.charAt
 
+      // alert(newUserInputSlice1);
 
     $("#return").click(function(){
       $("#puzzle").hide();
